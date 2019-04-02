7760ff186170467380e78e17fa67a4e580a2c0c9
ken2990 <49084357+ken2990@users.noreply.github.com>
2019-04-02 14:25:38 -0600

    Add files via upload

---
+++ Tarea1.py
@@ -0,0 +1,8 @@
+
+import math
+pi= math.pi
+
+my_string = 3.1415926
+
+
+print('* %.9s' % (my_string), '** %.8s' % (my_string), '*** %.7s' % (my_string), '**** %.6s' % (my_string), '***** %.5s' % (my_string), '****** %.4s' % (my_string))
