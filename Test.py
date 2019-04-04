6e8729117886e9e9ac2c290e776e509aa66282dc
ken2990 <49084357+ken2990@users.noreply.github.com>
2019-04-04 14:14:36 -0600

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,13 @@
+import math
+pi= math.pi
+
+my_string = 3.1415926
+
+my_variable = "*"
+
+
+
+print(my_variable*1, '%.9s' % (my_string), my_variable*2,' %.8s' % (my_string), my_variable*3,'%.7s' % (my_string), my_variable*4, ' %.6s' % (my_string), my_variable*5,' %.5s' % (my_string), my_variable*6,' %.4s' % (my_string))
+
+
+
